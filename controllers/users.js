04fd6a9cab86bd5c1a47a85c6e9491e739827a2a
Guanhao <daven.shen@gmail.com>
2014-07-01 23:15:16 +0800

    Update

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,25 @@
 module.exports = function(app) {
 /**
- * a home page route
+ * user profile page
  */
-  app.get('/users/signup', function(req, res) {
+  app.get('/users/profile', function(req, res){
+    res.render('users/profile',{
+			title			: 'My Profile',
+			bodyClass : 'page-sub-page page-profile page-account'
+    });
+  });
+/**
+ * sign up action
+ */
+  app.post('/users/signup', function(req, res) {
       // any logic goes here
-      res.render('users/signup')
+      
   });
 
 /**
- * About page route
+ * login action
  */
-  app.get('/users/login', function(req, res) {
+  app.post('/users/login', function(req, res) {
       // any logic goes here
       res.render('users/login')
   });
