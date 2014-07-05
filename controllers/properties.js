e6aaf1c66bf1ed8d4b722a718f4bc3ae6dc4d5d9
YiChenshu <ycswaves@gmail.com>
2014-07-05 20:58:23 +0800

    add some icons, wired up data using handlebar

--- controllers/properties.js
+++ controllers/properties.js
@@ -4,8 +4,57 @@ module.exports = function(app) {
  */
   app.get('/properties', function(req, res){
     res.render('properties/list',{
-	  title     : 'Property List',
-	  bodyClass : 'page-sub-page page-listing-lines page-search-results'
+	    title     : 'Property List',
+	    bodyClass : 'page-sub-page page-listing-lines page-search-results',
+      properties: getProperty()
     });
   });
+}
+
+function getProperty(){
+  dummy = [{
+    title:'blk486 帕尔马路',
+    price: 800,
+    apartmentType: 'HDB',
+    roomType: '普通房',
+    mrt: 'Braddle'
+  },{
+    title:'blk8 坎特贝利路',
+    price: 1800,
+    apartmentType: '公寓',
+    roomType: '主人房',
+    mrt: 'Seragoon'
+  },{
+    title:'blk248 墨菲场',
+    price: 1200,
+    apartmentType: '公寓',
+    roomType: '普通房',
+    mrt: 'Bishan'
+  },{
+    title:'blk12 布莱恩街',
+    price: 680,
+    apartmentType: 'HDB',
+    roomType: '主人房',
+    mrt: 'Bugis'
+  },{
+    title:'blk83 坎特贝利路',
+    price: 1100,
+    apartmentType: '公寓',
+    roomType: '主人房',
+    mrt: 'City Hall'
+  },{
+    title:'blk8 Rinehart',
+    price: 1300,
+    apartmentType: 'Condo',
+    roomType: '主人房',
+    mrt: 'Toa Payoh'
+  },{
+    title:'blk8 Tennessee Residence',
+    price: 3900,
+    apartmentType: '公寓',
+    roomType: '整套',
+    mrt: 'Toa Payoh'
+  }];
+
+  return dummy;
 }
--- models/property.js
+++ models/property.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema
 
   , propertySchema = new mongoose.Schema({
-      title: String,
-      price: Number,
+      title : String,
+      price : Number,
       room : Number,
+      userid : String,
       active  : Boolean,
       created : Date,
       updated : Date,
